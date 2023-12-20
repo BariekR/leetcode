e39413e7b8805ed4139480c9ad0fa63713db737c
Ramez Bariekzahy <ramez@seznam.cz>
2023-12-20 20:45:35 +0100

    ReverseString added

---
+++ leetcode/src/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        reverse(0, s.length - 1, s);
+    }
+
+    private void reverse(int start, int end, char[] s) {
+        if (start > end) {
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+        reverse(start + 1, end - 1, s);
+    }
+}
