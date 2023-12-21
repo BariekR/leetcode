f6b28723c80834390f6db2904043e53aa230e372
Ramez Bariekzahy <ramez@seznam.cz>
2023-12-21 19:11:05 +0100

    SwapNodesInPairs added

---
+++ leetcode/src/SwapNodesInPairs.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class SwapNodesInPairs {
+
+    /*public static void main(String[] args) {
+        ListNode ln4 = new ListNode(4, null);
+        ListNode ln3 = new ListNode(3, ln4);
+        ListNode ln2 = new ListNode(2, ln3);
+        ListNode ln1 = new ListNode(1, ln2);
+
+        swapPairs(ln1);
+    }*/
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head.next;
+        head.next = swapPairs(temp.next);
+        temp.next = head;
+        return temp;
+    }
+}
