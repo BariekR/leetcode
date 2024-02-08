858ccb196f2f0879bf72e13c95193d2e0b6617ad
Ramez Bariekzahy <ramez@seznam.cz>
2024-02-08 10:30:27 +0100

    PerfectSquares added

---
+++ leetcode/src/PerfectSquares.java
@@ -0,0 +1,20 @@
+
+public class PerfectSquares {
+
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                int remain = i - j * j;
+                if (dp[remain] < min) {
+                    min = dp[remain];
+                }
+            }
+            dp[i] = min + 1;
+        }
+        return dp[n];
+    }
+}
